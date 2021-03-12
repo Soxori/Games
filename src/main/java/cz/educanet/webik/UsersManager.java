e92ea65b6623822aed0f8aa8ae88a32d3d54563d
Jakub Panek <jakub.panek@educanet.cz>
2021-03-12 18:52:30 +0100

    sadsaasdas

---
+++ src/main/java/cz/educanet/webik/UsersManager.java
@@ -0,0 +1,11 @@
+package cz.educanet.webik;
+
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+@SessionScoped
+public class UsersManager implements Serializable {
+
+    User user = null;
+
+}
